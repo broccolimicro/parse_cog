e53cdc6c4ce435b0a322f75effac47a94316d1b6
Ned Bingham <edward.bingham@broccolimicro.io>
2025-04-25 15:52:43 -0400

    supporting variable declarations in cog parser

--- parse_cog/branch.cpp
+++ parse_cog/branch.cpp
@@ -5,39 +5,37 @@
 
 namespace parse_cog {
 
-branch::branch()
-{
-
+branch::branch() {
 }
 
-branch::branch(composition sub)
-{
+branch::branch(composition sub) {
 	this->sub = std::shared_ptr<composition>(new composition(sub));
 }
 
-branch::branch(control ctrl)
-{
+branch::branch(control ctrl) {
 	this->ctrl = std::shared_ptr<control>(new control(ctrl));
 }
 
-branch::branch(assignment assign)
-{
+branch::branch(assignment assign) {
 	this->assign = assign;
 }
 
-branch::~branch()
-{
-
+branch::branch(parse_ucs::declaration decl) {
+	this->decl = decl;
 }
 
-string branch::to_string(int level, string tab) const
-{
+branch::~branch() {
+}
+
+string branch::to_string(int level, string tab) const {
 	if (sub and sub->valid) {
 		return sub->to_string(level, tab);
 	} else if (ctrl and ctrl->valid) {
 		return ctrl->to_string(tab);
 	} else if (assign.valid) {
 		return tab + assign.to_string(tab);
+	} else if (decl.valid) {
+		return tab + decl.to_string(tab);
 	}
 	return tab + "skip";
 }
--- parse_cog/branch.h
+++ parse_cog/branch.h
@@ -3,27 +3,29 @@
 #include <parse/parse.h>
 #include <parse/syntax.h>
 #include <parse_expression/assignment.h>
+#include <parse_ucs/declaration.h>
 
 #include <memory>
 
-namespace parse_cog
-{
+namespace parse_cog {
+
 using parse_expression::assignment;
 
 struct composition;
 struct control;
 
-struct branch
-{
+struct branch {
 	branch();
 	branch(composition sub);
 	branch(control ctrl);
 	branch(assignment assign);
+	branch(parse_ucs::declaration decl);
 	~branch();
 
 	std::shared_ptr<composition> sub;
 	std::shared_ptr<control> ctrl;
 	assignment assign;
+	parse_ucs::declaration decl;
 
 	string to_string(int level, string tab) const;
 };
